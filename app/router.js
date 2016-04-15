75a2c5df5fd12b1c1d26977a293f498ef89bb7cf
akire0124 <oblena.erika@gmail.com>
2016-04-14 22:41:00 -0400

    Created the nav bar

--- app/router.js
+++ app/router.js
@@ -6,6 +6,14 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+	this.route('availablehounds');
+	this.route('prisonhounds');
+	this.route('donate');
+	this.route('adoption');
+	this.route('about');
+	this.route('adopted');
+	this.route('events');
+	this.route('shop');
 });
 
 export default Router;
